52287acf4696ff51010dc9b679b37d08dc6c928f
Sophia Taranets <64256842+SophiaTaranets@users.noreply.github.com>
2021-11-08 14:40:34 +0200

    Update homework8.js

--- Homework8/homework8.js
+++ Homework8/homework8.js
@@ -8,10 +8,12 @@
             console.log("String doesn`t start with uppercase character ");
         }
     }
+    console.log('Task 1');
     checkRegistr('regexp');
     checkRegistr('RegExp');
     checkRegistr('tegExp');
     checkRegistr('Rei');
+    console.log('\n');
 
 // **************** Task2 **************** 
 
@@ -19,21 +21,26 @@ function checkEmail (email){
     const regularExp = /\S+@\S+\.\S+/;
     console.log(regularExp.test(email));
 }
+console.log('Task 2');
 checkEmail("Qmail2@gmail.com");
+console.log('\n');
 
 // **************** Task3 **************** 
 
 const myRe = /d(b+)(d)/i;
 const myList = myRe.exec("cdbBdbsbz");
+console.log('Task 3');
 console.log(myList);
+console.log('\n');
 
 // **************** Task4 **************** 
 
 const reg = /(\w+)\s(\w+)/;
 const string = "Java Script";
 const newString = string.replace(reg,"$2, $1");
+console.log('Task 4');
 console.log(newString);
-
+console.log('\n');
 
 // **************** Task5 **************** 
 
@@ -41,29 +48,33 @@ function checkBankCard(str){
     const reExp = /^\d{4}-\d{4}-\d{4}-\d{4}$/;
     const res = reExp.test(str);
     if (res){
-        console.log("Validation succsesful!!");
+        console.log("Correct!");
     } else {
-        console.log("Please, try again. Incorrect data input");
+        console.log("False! Please, try again. Incorrect data!");
     }
 }
+console.log('Task 5');
 checkBankCard('1234-5678-9101-1213');
 checkBankCard('1234-5678c-9101-1213');
 checkBankCard('12342-5678-9101-1213');
+console.log('\n');
 
 // **************** Task6 ****************
 
 function checkEmail_(emeil_){
     const mailPattern = /^[a-zA-z0-9]+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/;
     if(mailPattern.test(emeil_)){
-        console.log("email is correct!");
+        console.log("Email is correct!");
     }
     else{
-        console.log("email is NOT correct!");
+        console.log("Email is NOT correct!");
     }
 } 
+console.log('Task 6');
 checkEmail_('my_mail@gmail.com');
 checkEmail_('#my_mail@gmail.com');
 checkEmail_('my_ma--il@gmail.com');
+console.log('\n');
 
 // **************** Task7 ****************
 
@@ -74,7 +85,7 @@ function checkLogin(login){
     const result = re.test(login);
     console.log(`${login} is ${result}-${resultNumber}`);
 }
-
+console.log('Task 7');
 checkLogin('ee1.1ret3');
 checkLogin('ee1*1ret3');
-
+console.log('\n');
